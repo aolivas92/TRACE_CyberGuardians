cf85529300566efe9921dfec8ab2556e4911914e
aolivas92 <olivas9215@hotmail.com>
2025-04-02 11:11:21 -0600

    Revert "Pushing initial configuration from frontend to backend then to crawler"
    This reverts commit 53b63d581b8542bc0509a84ecc1c5cfdf2ae5103.

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, Request, BackgroundTasks, HTTPException
-from fastapi import FastAPI, Request, BackgroundTasks, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Optional
