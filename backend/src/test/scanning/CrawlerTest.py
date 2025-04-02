ba494054bac6e12751a9311c57b189a63a4c5ebf
aolivas92 <olivas9215@hotmail.com>
2025-04-01 23:09:05 -0600

    Updating the import with the new crawler_manager name

--- backend/src/test/scanning/CrawlerTest.py
+++ backend/src/test/scanning/CrawlerTest.py
@@ -1,7 +1,7 @@
 import asyncio
 import pytest
 from unittest.mock import AsyncMock, patch
-from src.modules.scanning.CrawlerManager import CrawlerManager
+from src.modules.scanning.crawler_manager import crawler_manager
 
 @pytest.mark.asyncio
 async def test_crawler_manager():
@@ -9,7 +9,7 @@ async def test_crawler_manager():
     mock_http_client.get.side_effect = ["<html><body><a href='/level1/page1'>Page 1</a></body></html>",
         "<html><body>No more links here.</body></html>"]
     with patch("crawler_manager.RealHTTPClient", return_value=mock_http_client):
-        manager = CrawlerManager()
+        manager = crawler_manager()
         manager.configure_crawler(target_url="http://localhost:5000/", depth=1, limit=10, user_agent="Mozilla/5.0", delay=0, proxy=None,)
         results = await manager.start_crawl()
     assert len(results) == 2
